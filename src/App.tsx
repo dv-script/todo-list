bbb33c4a0d16c8935dead94ae8a76f005e5e748b
Davi Hasson Castro <davihasson@gmail.com>
2023-07-21 13:10:26 -0300

    chore: adding to App all components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,44 @@
+import { GlobalStyle } from "./GlobalStyles";
+
+import AddNewTask from "./components/AddNewTask";
+import Header from "./components/Header";
+import ToDoList from "./components/ToDoList";
+
+export interface ITaskList {
+  id: number;
+  task: string;
+  completed: boolean;
+}
+
+const taskList: ITaskList[] = [
+  {
+    id: 1,
+    task: "Estudar React",
+    completed: true,
+  },
+  {
+    id: 2,
+    task: "Estudar Typescript",
+    completed: false,
+  },
+  {
+    id: 3,
+    task: "Estudar Node",
+    completed: true,
+  }
+];
+
 export default function App() {
   return (
     <>
-      
+      <GlobalStyle />
+      <Header />
+      <AddNewTask 
+        taskList={taskList}
+      />
+      <ToDoList
+        taskList={taskList}
+      />
     </>
-  )
+  );
 }
